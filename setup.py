ebe24335c8b2f51fc7a9bd385c605aee384b3d85
Robert Wikman <rbw@vault13.org>
2019-06-28 19:03:36 +0200

    init

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with codecs.open("README.rst", encoding="utf-8") as f:
     readme = f.read()
 
 setup(
-    name="alabaster",
+    name="aioli_sphinx_theme",
     version=version,
     description="A configurable sidebar-enabled Sphinx theme",
     long_description=readme,
@@ -23,25 +23,5 @@ setup(
     url="https://alabaster.readthedocs.io",
     packages=["alabaster"],
     include_package_data=True,
-    entry_points={"sphinx.html_themes": ["alabaster = alabaster"]},
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Documentation",
-        "Topic :: Software Development :: Documentation",
-    ],
+    entry_points={"sphinx.html_themes": ["aioli_sphinx_theme = alabaster"]},
 )
